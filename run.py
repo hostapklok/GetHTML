a3783cc548f98f5a73befb840c83004d9f217bd5
hostapklok <hostapklok@gmail.com>
2025-06-04 00:05:41 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,4 +2,5 @@ import os
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("server:app", port=os.getenv("PORT", 5555))
+    port = int(os.getenv("PORT", 5555))
+    uvicorn.run("server:app", host="0.0.0.0", port=port)
